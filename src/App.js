0aa393f41c0675e69ee59da596b02f4306e72f50
artenlf <felipe.arten@gmail.com>
2022-10-14 11:10:30 -0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import listOfProducts from "./db.json";
+
+const products = listOfProducts;
 
 function App() {
+  const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState("");
+
+  useEffect(() => {
+    const sortArray = (type) => {
+      const types = {
+        id: "id",
+        name: "name",
+        inverseName: "inverseName",
+        price: "price",
+        highestPrice: "highestPrice",
+      };
+      const sortProperty = types[type];
+      const sorted = [...products].sort((a, b) => {
+        if (sortProperty === "name") {
+          return a.name.localeCompare(b.name);
+        }
+        if (sortProperty === "inverseName") {
+          return b.name.localeCompare(a.name);
+        }
+        if (sortProperty === "highestPrice") {
+          return b.price - a.price;
+        } else {
+          return a[sortProperty] - b[sortProperty];
+        }
+      });
+      setData(sorted);
+    };
+    sortArray(sortType);
+  }, [sortType]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="title">Dynamic selector</h1>
+      <div className="selectorContainer">
+        <select
+          className="selector"
+          onChange={(event) => setSortType(event.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option>id</option>
+          <option value="name">A-Z</option>
+          <option value="inverseName">Z-A</option>
+          <option value="price">Lowest price</option>
+          <option value="highestPrice">Highest price</option>
+        </select>
+      </div>
+      <div className="grid">
+        {data.map((item) => {
+          return (
+            <div className="item" key={item.id}>
+              <img src={item.picture} alt={item.name} />
+              <div>{item.id}</div>
+              <div>{item.name}</div>
+              <div>{item.price}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
